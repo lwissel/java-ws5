af5400030fb62f706153d2845d4b1074544ef85e
lwissel <lennart.wissel@gmx.net>
2014-11-29 20:02:07 +0000

    add basic runner

---
+++ Runner.java
@@ -0,0 +1,19 @@
+/** Exercise 1 ws5
+ *
+ * Runner class for testing
+ *
+ * @author  Lennart Wissel
+ * @version 29.11.2014
+ */
+
+public class Runner {
+
+  public static void main(String[] args) {
+    Person tom = new Person("tom", "m", 22);
+    Person ann = new Person("ann", "f", 22);
+    Person joe = new Person("joe", "m", 23);
+    Person mary = new Person("mary", "f", 23);
+    Person tim = new Person("tim", "m", 24);
+
+  }
+}
