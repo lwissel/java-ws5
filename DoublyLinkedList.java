4750de15bc07272c63c97b14e43fa9126b434c7a
lwissel <lennart.wissel@gmx.net>
2014-11-29 20:02:21 +0000

    experiment to make this work

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -7,9 +7,57 @@
  */
 
 public class DoublyLinkedList {
-  private Person nodeValue;
-  private DoublyLinkedList right;
-  private boolean empty;
+
+  /**
+   * Nested inner class Node to represent previous and next elements
+   */
+  private class Node {
+    private Person person;
+    private Node nextNode;
+    private Node previousNode;
+
+    /** full constructor
+     * @param person Person
+     * @param nextNode next Node
+     * @param previousNode previous Node
+     */
+    public Node(Node nextNode, Person person, Node previousNode) {
+      this.nextNode = nextNode;
+      this.person = person;
+      this.previousNode = previousNode;
+    }
+
+    /** start constructor
+     * @param person generates node [null - person - null]
+     */
+    public Node(Person person) {
+      this(null, person, null);
+    }
+
+    /** GETTERS
+     * @return node of next node
+     */
+    public Node getNextNode() {
+      return this.nextNode;
+    }
+    /**
+     * @return node of previous node
+     */
+    public Node getPreviousNode() {
+      return this.previousNode;
+    }
+
+  } // end inner class
+
+  // fields for DLL class
+  private Node node;
+  private Node nextNode;
+  private Node previousNode;
+  private Node firstNode;
+  private Node lastNode;
+
+  private DoublyLinkedList rightDLL;
+  private DoublyLinkedList left;
 
 
   /** full constructor
@@ -17,26 +65,46 @@ public class DoublyLinkedList {
    * @param nodeValue is the node value
    * @param right is the dll to the right
    */
-  public DoublyLinkedList(Person nodeValue, DoublyLinkedList right) {
-    this.nodeValue = nodeValue;
-    this.right = right;
-    this.empty = false;
+  public DoublyLinkedList(Person person, DoublyLinkedList rightDLL) {
+    if(rightDLL == null) { // no previous node exists therefore create a new null-person-null node
+      this.node = new Node(person);
+      this.firstNode = this.node; // this will be the firstNode for the whole DLL
+      this.lastNode = this.node; // set the current node to be the last node, this will change
+    }
+    if (rightDLL != null) { // there already exists a node therefore we just add a node to the dll
+      this.firstNode = rightDLL.getFirstNode();
+      this.node = new Node(null, person, rightDLL.getLastNode()) ;
+      this.lastNode = node; // will be the lastNode of the next recursive call in the line above
+    }
   }
 
-  /** empty constructor to represent null elements
-   * we have to throw exceptions in getters and setters if left or right are null
+  /**
+   * empty constructor returns null
    */
   public DoublyLinkedList() {
-    this.empty = true;
+    this.rightDLL = null;
+  }
+
+  /** GETTER
+   * @return the first node of the DLL
+   */
+  public Node getFirstNode() {
+    return this.firstNode;
+  }
+  /**
+   * @return the last node of the DLL
+   */
+  public Node getLastNode() {
+    return this.lastNode;
   }
 
   /** static method to construct DLLs
-   * @param p Person
+   * @param person Person
    * @param right is the DLL to the right
    * @return a new DLL
    */
-  public static DoublyLinkedList cons(Person p, DoublyLinkedList right) {
-    return new DoublyLinkedList(p,right);
+  public static DoublyLinkedList cons(Person person, DoublyLinkedList right) {
+    return new DoublyLinkedList(person,right);
   }
 
   /** static method to represent the end of the list
